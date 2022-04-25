e453a17920ff910fa3f0b9d288613ebc04b791a8
Kelner Roberto <kelner250@hotmail.com>
2022-04-25 13:42:05 -0300

    adiciona react router dom - modifica o app para receber as rotas - faz teste de rota com home e cria um componente para ser renderizado

---
+++ poke-test/src/components/HomeComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HomeComponent: React.FC = () => {
+  return (
+    <div>
+      <p>Hello World! alterando home component</p>
+    </div>
+  );
+}
---
+++ poke-test/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HomeComponent } from '../components/HomeComponent';
+
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <HomeComponent />
+    </div>
+  );
+}
