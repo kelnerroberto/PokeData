238eccdfcd4904081836219ff26189be48713c8e
Kelner Roberto <kelner250@hotmail.com>
2022-04-25 14:01:25 -0300

    cria a função para chamar os pokemons (ainda está estático

---
+++ poke-test/src/API/fetchFunctions/MainFetch.ts
@@ -0,0 +1,15 @@
+interface returnedFromAPI {
+  count: number;
+  next: string;
+  previous: string | null;
+  results: Array<{
+    name: string;
+    url: string;
+  }>;
+}
+
+export const fetchPokemonsForHomePage = async (): Promise<returnedFromAPI> => {
+  const apiCall = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=10');
+  const pokemonsJson = await apiCall.json();
+  return pokemonsJson;
+}
